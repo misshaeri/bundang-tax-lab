b898b333221cf910d16124232a49fa7590b479dc
misshaeri <misshaeri@gmail.com>
2026-09-08 15:55:41 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,15 +1,1 @@
-const CACHE='bundang-tax-lab-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
-});
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => {
-  if (e.request.method !== 'GET') return;
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).then(resp => {
-    const clone=resp.clone();
-    caches.open(CACHE).then(c => c.put(e.request, clone)).catch(()=>{});
-    return resp;
-  }).catch(() => cached)));
-});
+const C='taxlab-v8-5-total-bundang';const A=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
